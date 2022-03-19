49479e797c1433603e2a0a67f2957d168e2ad8a3
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-03-19 16:51:59 -0400

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  bae: 'https://cesarionivar.github.io/',
+  bae: 'https://cesarionivar.github.io/gha-react-vite/',
   plugins: [react()],
 });
